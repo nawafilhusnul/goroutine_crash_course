884252fb8d3572126e8483b03fcdbe4b7187c4ff
nawafilhusnul <nawafilhusnul@gmail.com>
2023-09-23 12:45:26 +0700

    feat: producer and consumers problem

--- main.go
+++ main.go
@@ -2,37 +2,27 @@ package main
 
 import (
 	"fmt"
-	"goroutine/challenge_1"
-	"goroutine/mutex"
 	"sync"
 )
 
 func main() {
-	var operation = "mutex"
-	switch operation {
-	case "challenge_1":
-		challenge_1.Challenge1()
-	case "mutex":
-		mutex.Main()
-	default:
-		words := []string{
-			"a",
-			"b",
-			"c",
-			"d",
-			"e",
-			"f",
-		}
-		wg := sync.WaitGroup{}
 
-		wg.Add(len(words))
-		for i, x := range words {
-			go printSomething(&wg, fmt.Sprintf("%d : %s", i, x))
-		}
+	words := []string{
+		"a",
+		"b",
+		"c",
+		"d",
+		"e",
+		"f",
+	}
+	wg := sync.WaitGroup{}
 
-		wg.Wait()
+	wg.Add(len(words))
+	for i, x := range words {
+		go printSomething(&wg, fmt.Sprintf("%d : %s", i, x))
 	}
 
+	wg.Wait()
 }
 
 func printSomething(wg *sync.WaitGroup, s string) {
---
+++ producer_consumer/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"math/rand"
+	"time"
+)
+
+const NumberOfPizzas = 10
+
+var pizzasMade, pizzasFailed, total int
+
+type Producer struct {
+	data chan PizzaOrder
+	quit chan chan error
+}
+
+type PizzaOrder struct {
+	pizzaNumber int
+	message     string
+	success     bool
+}
+
+func (p *Producer) Close() error {
+	ch := make(chan error)
+
+	p.quit <- ch
+
+	return <-ch
+}
+
+func makePizza(pizzaNumber int) *PizzaOrder {
+	pizzaNumber++
+	if pizzaNumber <= NumberOfPizzas {
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Received order number #%d!\n", pizzaNumber)
+
+		rnd := rand.Intn(12) + 1
+		msg := ""
+		success := false
+
+		if rnd < 5 {
+			pizzasFailed++
+		} else {
+			pizzasMade++
+		}
+		total++
+
+		fmt.Printf("Making pizza #%d. It will take %d seconds....\n", pizzaNumber, delay)
+
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("*** We ran out of ingredients for pizza #%d!", pizzaNumber)
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("*** The cook quit while making pizza #%d!", pizzaNumber)
+		} else {
+			success = true
+			msg = fmt.Sprintf("Pizza order is ready!")
+		}
+
+		return &PizzaOrder{
+			pizzaNumber: pizzaNumber,
+			message:     msg,
+			success:     success,
+		}
+	}
+
+	return &PizzaOrder{
+		pizzaNumber: pizzaNumber,
+	}
+}
+
+func pizzeria(pizzaMaker *Producer) {
+	//	keep track of which pizza we are making
+	var i = 0
+
+	//this loop will continue to execute, trying to make pizzas,
+	//until the quit channel receives something.
+	for {
+		currentPizza := makePizza(i)
+		if currentPizza != nil {
+			i = currentPizza.pizzaNumber
+
+			select {
+			//we tried to make a pizza (we send something to the data channel)
+			case pizzaMaker.data <- *currentPizza:
+			case quitChan := <-pizzaMaker.quit:
+				//	close channels
+				close(pizzaMaker.data)
+				close(quitChan)
+				return
+			}
+		}
+	}
+}
+
+func main() {
+	//  seed the random number generator
+	rand.New(rand.NewSource(time.Now().UnixNano())).Int63()
+
+	//	print out a message
+	color.Cyan("The Pizzeria is open for business!")
+	color.Cyan("----------------------------------")
+
+	//	create a producer
+	pizzaJob := &Producer{
+		data: make(chan PizzaOrder),
+		quit: make(chan chan error),
+	}
+
+	//	run the producer n the background
+	go pizzeria(pizzaJob)
+
+	//	create and run consumer
+	for i := range pizzaJob.data {
+		if i.pizzaNumber <= NumberOfPizzas {
+			if i.success {
+				color.Green(i.message)
+				color.Green("Order #%d is out for delivery!", i.pizzaNumber)
+			} else {
+				color.Red(i.message)
+				color.Red("The customer is really mad!")
+			}
+		} else {
+			color.Cyan("Done making pizzas ....")
+
+			err := pizzaJob.Close()
+			if err != nil {
+				color.Red("*** Error closing channel!", err)
+			}
+		}
+	}
+
+	//	print out the ending message
+	color.Cyan("--------------------------------")
+	color.Cyan("Done for teh day! We are closed!")
+
+	color.Cyan("We made %d pizzas, but failed to make %d, with %d attempts in total.", pizzasMade, pizzasFailed, total)
+
+	switch {
+	case pizzasFailed > 9:
+		color.Red("It was an awful day...")
+	case pizzasFailed > 6:
+		color.Red("It was not a very good day...")
+	case pizzasFailed > 4:
+		color.Yellow("It was an okay day...")
+	case pizzasFailed > 2:
+		color.Yellow("It was a pretty good day...")
+	default:
+		color.Green("It was awesome day...")
+	}
+}
